f9366dfd4ffb7836780d1ac1f991d905d6141e9c
Keagan <02keagan.worm@gulllakecs.org>
2022-02-07 14:16:32 -0500

    IMPORT SLEEP FROM TIME LIBRARY

--- Welcome_InfoTechCenter.py
+++ Welcome_InfoTechCenter.py
@@ -1,5 +1,9 @@
 # Code Name - Hornet
 
+from time import sleep  #print to one line with time delay between prints
+
+
 print("Welcome to Hornets InfoTechCenter\n")
+sleep(1)
 
 print("Hornets operating system booting up")
