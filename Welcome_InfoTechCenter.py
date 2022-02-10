6fe78e3181bbebd4426d651250a14474cf51adf1
Keagan <02keagan.worm@gulllakecs.org>
2022-02-10 14:00:22 -0500

    Added some color to my text

--- Welcome_InfoTechCenter.py
+++ Welcome_InfoTechCenter.py
@@ -1,9 +1,11 @@
 # Code Name - Hornet
 
-from time import sleep  #print to one line with time delay between prints
+from time import sleep #Print to one line with time delay between print
 
+print("\033[1;32m loading... \n")
+sleep(1)
 
 print("Welcome to Hornets InfoTechCenter\n")
 sleep(1)
 
-print("Hornets operating system booting up")
+print("Hornet's Operating System Booting Up\n")
